bf9a4f970bed0ed1ed6803735b4d704ba010c2c7
KlimenkoVera <klimenkovera1991@gmail.com>
2022-08-27 14:08:12 +0300

    Произведен анализ окончаний слов

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -2,5 +2,9 @@ package ru.netology
 
 fun main () {
 
+    val likes = 4
+
+    val peopleOrPerson = if (likes % 10 === 1 && likes % 100 !== 11) "человеку" else "людям"
+    println("Понравилось $likes $peopleOrPerson")
 
 }
